635049780df2f3c6fcd296a0918b4218a5ba6489
00-00-00\yustu <yustus.misha10@gmail.com>
2024-06-26 12:02:07 +0300

    no message

--- utils/neuro_models/meta.py
+++ utils/neuro_models/meta.py
@@ -5,7 +5,7 @@ import replicate
 from utils.functions import text_formating
 from utils.translator import deep_translator
 
-os.environ["REPLICATE_API_TOKEN"] = "r8_XwSodDnHucmy5T3HbQOPXvLl2IDwZhG2VE72v"
+os.environ["REPLICATE_API_TOKEN"] = "r8_JSzuYNYvHQNHrD1Mtxt0Bso2Yc545Zr1dxXis"
 
 
 def meta_ai(prompt):
