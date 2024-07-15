63c8a37e9723bd23d326606a6945c4393b9a25cc
00-00-00\yustu <yustus.misha10@gmail.com>
2024-07-15 23:59:37 +0300

    no message

--- keyboards/users/inline/form_keyboards.py
+++ keyboards/users/inline/form_keyboards.py
@@ -7,3 +7,9 @@ categ_buttons = [InlineKeyboardButton(text=category, callback_data=category) for
 first_kb = InlineKeyboardMarkup(row_width=2)
 
 first_kb.add(*categ_buttons)
+
+back_button = InlineKeyboardButton(text="Проверить другие запросы", callback_data="back")
+
+back_kb = InlineKeyboardMarkup(row_width=1)
+
+back_kb.add(back_button)
