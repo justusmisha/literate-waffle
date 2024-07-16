162921c40b765c16b73f4b587d9cfdd48405eae7
00-00-00\yustu <yustus.misha10@gmail.com>
2024-07-16 18:54:16 +0300

    no message

--- utils/functions.py
+++ utils/functions.py
@@ -12,28 +12,26 @@ def text_formating(text):
 
 
 def google_prompts(category):
-    try:
-        spreadsheet_id = '1mlNDWs__ncSwFBGl6xQkwKUQj9RWSQfsvIvxh_CCh5g'
-        range_name = f'Промпты!A1:B4'
-        creds = Credentials.from_authorized_user_file(
-            'token.json',
-            scopes=['https://www.googleapis.com/auth/spreadsheets']
-        )
+    spreadsheet_id = '1mlNDWs__ncSwFBGl6xQkwKUQj9RWSQfsvIvxh_CCh5g'
+    range_name = f'Промпты!A1:B4'
+    creds = Credentials.from_authorized_user_file(
+        'token.json',
+        scopes=['https://www.googleapis.com/auth/spreadsheets']
+    )
 
-        service = build('sheets', 'v4', credentials=creds)
+    service = build('sheets', 'v4', credentials=creds)
 
-        sheet = service.spreadsheets()
-        result = sheet.values().get(spreadsheetId=spreadsheet_id, range=range_name).execute()
-        values = result.get('values', [])
-        if not values:
-            raise ValueError(f'Category "{category}" not found.')
-        else:
-            for row in values:
-                if row[0] == category:
-                    return row[1]
-            raise ValueError(f'Category "{category}" not found.')
-    except Exception as e:
-        print(e)
+    sheet = service.spreadsheets()
+    print(sheet)
+    result = sheet.values().get(spreadsheetId=spreadsheet_id, range=range_name).execute()
+    values = result.get('values', [])
+    if not values:
+        raise ValueError(f'Category "{category}" not found.')
+    else:
+        for row in values:
+            if row[0] == category:
+                return row[1]
+        raise ValueError(f'Category "{category}" not found.')
 
 
 def get_call_to_action():
