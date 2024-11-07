ef7e13cd15c072dc10b36559f3e0840d9b8a992c
Justus Misha <90780967+justusmisha@users.noreply.github.com>
2024-11-07 14:19:18 +0300

    Update yandexGPT.py

--- utils/neuro_models/yandexGPT.py
+++ utils/neuro_models/yandexGPT.py
@@ -28,7 +28,8 @@ async def yandexGPT(prompt):
     url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
     headers = {
         "Content-Type": "application/json",
-        "Authorization": "Api-Key AQVN2AUOMm-n8cPEqq9HiTlf_Jy-lU-xXdqLUpTS"
+        "Authorization": "Api-Key AQVN2AUOMm-n8cPEqq9HiTlf_Jy-lU-xXdqLUpTS",
+        "x-folder-id": "b1gi45iplkdieuk7imj9"
     }
 
     response = requests.post(url, headers=headers, json=prompt)
